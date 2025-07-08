5ae35578f3ded39ddbea34ae990ea3111736d1e2
GuyOrShay <guysh@infraedge.co.il>
2025-07-08 14:05:52 +0300

    init commit

---
+++ src/app/profile/education/education.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.scss']
+})
+export class EducationComponent implements OnInit {
+  educationData: any;
+  constructor(private profileService:ProfileService) { }
+
+  ngOnInit() {
+    this.educationData = this.profileService.educationData
+  }
+
+}
